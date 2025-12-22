7378c033e682eb191e5ebf91230769acf8bbd6c3
Ricardo Freccero <ricardofreccero@hotmail.com>
2025-12-22 05:23:43 +0000

    feat: agregar soporte web

---
+++ web.py
@@ -0,0 +1,14 @@
+import os
+import flet as ft
+from src.main_app import main
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    print(f"Iniciando en modo WEB en el puerto {port}...")
+
+    ft.app(
+            target=main,
+            view=ft.WEB_BROWSER,
+            port=port,
+            host="0.0.0.0"
+            )
